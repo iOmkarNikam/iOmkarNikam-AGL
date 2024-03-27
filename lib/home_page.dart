f0d0290f8b26fbc10b74b06c0ca1e82313fb78ee
iOmkarNikam <omkar.nikam.2026@gmail.com>
2024-03-27 22:35:36 +0530

    Day2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "DEVOM";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quiz for AGL"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("My Name is Omkar Nikam"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child:Center(
-         child:Container(
-          child: Text("My Name is Omkar Nikam"),
-         ),
-      )
-     )
+      home: HomePage(),
     );
-
   }
 }
